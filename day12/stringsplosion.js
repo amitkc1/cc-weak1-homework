b883a02633d6d680bb78077b68ae0fb9953ca346
amitkc <amitkcpvg@gmail.com>
2015-07-23 19:19:24 -0500

    Committing day12 warmup and RPG-console

---
+++ day12/stringsplosion.js
@@ -0,0 +1,12 @@
+var readline = require('readline-sync');
+var answer = readline.question("Enter your string:");
+splosion(answer);
+
+function splosion(answer){
+    var newAnswer="";
+    while(answer.length>=1){
+        newAnswer=answer.concat(newAnswer);
+        answer = answer.substring(0,answer.length-1);
+    }
+    console.log("Your stringsplosion is: "+newAnswer);
+}
--- day9/RPG-Console.js
+++ day9/RPG-Console.js
@@ -1,25 +1,37 @@
 var readlineSync = require('readline-sync');
-var enemies =['Fierce Demon','Ugly Wargile','Weak skeleton'];
-var enemy = function(name,health){
+var enemies ={};
+var randomEnemy=0;
+var power=0;
+var attackProbability =0;
+var Enemy = function(name,health){
     this.name = name;
     this.health = health;
 }
+var player = function(name,health){
+    this.name = name;
+    this.health = health;
+}
+var myPlayer = new player("Mario",500);
+enemies[0] = new Enemy('Fierce Demon',200);
+enemies[1] = new Enemy('Ugly Wargile',200);
+enemies[2] = new Enemy('Weak Skeleton',200);
 
-var fierceDemon = new Enemy('Fierce Demon',100);
-var uglyWargile = new Enemy('Ugly Wargile',100);
-var weakSkeleton = new Enemy('Weak Skeleton',100);
-
-console.log("Welcome to nobody cares. Population:6 billion");
-readlineSync.question('Press a key to walk');
+console.log("Welcome to nobody cares. Population: 6 billion");
+var answer = readlineSync.question('Press a key to walk');
+if (answer.toLocaleLowerCase()==='print'){
+    console.log(myPlayer);
+}
+    
 generateEnemy();
 
 var input = readlineSync.question("Do you want to attack or run ?");
 if(input=='attack'){
-    var power = generateNumber(100,50);
+    power = generateNumber(100,50);
+    attackEnemy(power);
 }
 
 if(input=='run'){
-    var attackProbability = generateNumber(1,2);
+    attackProbability = generateNumber(1,2);
     if(attackProbability===1){
         console.log("Escaping");
     }
@@ -28,14 +40,37 @@ if(input=='run'){
     }
 }
 function generateEnemy(){
-    var randomEnemy = enemies[Math.floor(Math.random() * enemies.length)];
-    console.log(randomEnemy+" has appeared");
+    randomEnemy = Math.floor(Math.random() * ((enemies.length-1) - 0 + 1)) + 0;
+    console.log(enemies[randomEnemy].name+" has appeared");
 };
 
 function generateNumber(max,min){
     return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-function attackEnemy(){
+function attackEnemy(power){
+    enemies[randomEnemy].health -= power;
+    if(enemies[randomEnemy].health<0){
+        console.log("Enemy "+enemies[randomEnemy].name+" killed. Mario gets additional power");
+        myPlayer.health= myPlayer.health+50;
+    }
+    else{
+        console.log("Attacked enemy "+enemies[randomEnemy].name+" with "+power);
+        power = generateNumber(100,50);
+        attackCharacter(power);
+    }
+    
+}
+
+function attackCharacter(power){
+    myPlayer.health -= power;
+    if(myPlayer.health>0){
+        console.log("Enemy attacked player "+myPlayer.name+" with "+power);
+        power= generateNumber(50,100);
+        attackEnemy(power);
+    }
+    else{
+        console.log("Man down..! Oh, well. He wasn't that good anyway..! :)");
+    }
     
 }
