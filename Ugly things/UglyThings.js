d00eb58a9954e06b8e8a40f7a8701b9b3c3995aa
Amit Chaudhari <amitkcpvg@gmail.com>
2015-07-27 17:22:17 -0500

    Adding comment functionality

--- Ugly things/UglyThings.js
+++ Ugly things/UglyThings.js
@@ -1,13 +1,17 @@
 var app = angular.module('uglyThings',[]);
 app.controller('inputCtrl',function($scope){
     $scope.photos =[];
+    $scope.counter =0;
     $scope.submit = function(){
         $scope.photos.push({
             src: ($scope.URL),
             title:($scope.Title),
             caption: ($scope.description),
+            comments:[]
         });
         
+        $scope.counter++;
+            
     };
     
     $scope.remove = function(index){
@@ -15,9 +19,16 @@ app.controller('inputCtrl',function($scope){
         
     };
     
-    $scope.addComment = function(){
-        $scope.photos.comments = $scope.commentInput;
-        $scope.imageComment =$scope.commentInput;  
+    $scope.removeComment = function(index,commentIndex){
+        $scope.photos[index].comments.splice(commentIndex,1);
+    }
+    
+    $scope.checkCounter = function(){
+            return $scope.counter>0;
+    };
+    
+    $scope.addComment = function(index,commentInput){
+        $scope.photos[index].comments.push(commentInput);
     };
     
 });
