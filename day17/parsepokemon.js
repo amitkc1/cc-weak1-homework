f5d42ef0e571dae28441e1bfa815229794a20a01
amitkc <amitkcpvg@gmail.com>
2015-08-04 00:03:01 -0500

    Parse json file

--- day17/parsepokemon.js
+++ day17/parsepokemon.js
@@ -1,16 +1,29 @@
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+//var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+//
+//function parsePokemon(urlpath){
+//    var request = new XMLHttpRequest();
+//    request.open('GET', urlpath, false);
+//    request.send();
+//    if (request.status >= 200 && request.status < 400){
+//        var data = JSON.parse(request.responseText);
+//    }
+//    
+//    else{
+//        console.log("An error occurred"+request.status);
+//    }
+//}
+//
+//parsePokemon('./pokemon.json');
 
-function parsePokemon(urlpath){
-    var request = new XMLHttpRequest();
-    request.open('GET', urlpath, false);
-    request.send();
-    if (request.status >= 200 && request.status < 400){
-        var data = JSON.parse(request.responseText);
+
+var fs = require('fs');
+fs.readFile('pokemon.json', 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+  var obj = JSON.parse(data);
+    for(var number =0;number<obj.objects[0].pokemon.length;number++){
+        console.log(obj.objects[0].pokemon[number].name);
     }
     
-    else{
-        console.log("An error occurred"+request.status);
-    }
-}
-
-parsePokemon('./pokemon.json');
+});
