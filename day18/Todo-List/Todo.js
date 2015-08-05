fdab7c57c7685dc2d84377f413d1ed69ccd02fab
Amit Chaudhari <amitkcpvg@gmail.com>
2015-08-05 18:20:17 -0500

    basic working model of to-do list

--- day18/Todo-List/Todo.js
+++ day18/Todo-List/Todo.js
@@ -3,42 +3,59 @@ app.controller('todoCtrl',function($scope,$http){
     $scope.endPoint="http://mean.codingcamp.us/todo/amitc";
     $scope.response=""; 
     $scope.itemList=[];
+    $scope.strikeList = [];
     
     $scope.add = function(){
      
         if($scope.itemList.indexOf($scope.todoItem)==-1){
             $scope.itemList.push($scope.todoItem); 
-            $scope.postList($scope.todoItem);
-             
+            $scope.postList($scope.todoItem);             
         }
         
-    $scope.check = function(){
-            
+    }
+    
+    $scope.remove = function(index){
+        $scope.deleteItem($scope.itemList[index]);
+        $scope.listItems.splice(index,1);
         
-        }
+    }
+
+    $scope.check = function(index){
+        $scope.strikeList.push($scope.itemList[index]); 
+        $scope.listItems.splice(index,1);
     }
  
     
     $scope.postList = function(item){
+        newlistItem ={
+            title:item,
+            description: item+"--abc",
+            completed:true,
+            price:$scope.todoItemPrice,
+            imgUrl:$scope.todoItemImage
+        }
+        
+        $http.post($scope.endPoint,newlistItem).success(success).error(error);
+    }
+    
+    $scope.deleteItem = function(item){
         
         newlistItem ={
             title:item,
             description: item+"--abc",
-            completed:true
+            completed:true,
+            price:$scope.todoItemPrice,
+            imgUrl:$scope.todoItemImage
         }
-        
-        $http.post($scope.endPoint,newlistItem).success(postSucess).error(error);
+        $http.delete($scope.endPoint,newlistItem).success(success).error(error);
     }
     
     $scope.getList = function(){
-        $scope.response=$http.get($scope.endPoint).success(getSuccess).error(error);
+        $scope.response=$http.get($scope.endPoint).success(success).error(error);
      }
     
-     var getSuccess = function(data) {
-         $scope.response=data;
-    };
     
-    var postSucess = function(data){
+    var success = function(data){
         $scope.listItems=$scope.itemList;
         console.log(data);
     }
